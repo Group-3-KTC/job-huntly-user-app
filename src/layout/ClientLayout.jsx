dcbf8d5f241632d84ab42c5fcfae98daf7f2afd9
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-07-21 16:54:28 +0700

    create layout

---
+++ src/layout/ClientLayout.jsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
+
+export default function ClientLayout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen text-gray-900 bg-gray-100">
+      <Header />
+      <main className="flex-grow w-full px-4 py-6 mx-auto max-w-7xl">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
