fcfae63d68417152bca9a704b56df9403396c411
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-07-22 15:02:40 +0700

    Resolve stash merge and apply changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import HeroSection from "@/components/home/HeroSection";
+import StatsSection from "@/components/home/StatsSection";
+import CategoriesSection from "@/components/home/CategoriesSection";
+import VacanciesSection from "@/components/home/VacanciesSection";
+import HowItWorksSection from "@/components/home/HowItWorksSection";
+import FeaturedJobsSection from "@/components/home/FeaturedJobsSection";
 
 export default function HomePage() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <p className="text-gray-700">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. ...
-      </p>
+      <HeroSection />
+      <StatsSection />
+      <CategoriesSection />
+      <VacanciesSection />
+      <HowItWorksSection />
+      <FeaturedJobsSection />
     </div>
   );
-};
+}
